3a256736146682f02b06e9ec6508774895558fb8
Muhammad Ramadhan <muhrama082@gmail.com>
2019-08-07 22:38:44 +0700

    konfigurasi view

---
+++ resources/views/guest/index.blade.php
@@ -0,0 +1,9 @@
+@extends('template/main')
+
+@section('title','General Article')
+
+@section('main')
+<div class="container">
+    <h1>Test</h1>
+</div>
+@endsection
---
+++ resources/views/template/footer.blade.php
@@ -0,0 +1,3 @@
+<!-- <script src="{{url('assets/js/bootstrap.js')}}"></script>>
+    </body>
+</html> -->
